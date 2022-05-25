19ab3a4feb7ca711c1b57db9b5778efea25ce632
JCAlways <zhangsiz@vip.qq.com>
2022-05-25 14:41:19 +0800

    修改路由 添加404

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Vue3-ElementPlus-Ts-Template
 
-Vue3 + Vite + ElementPlus + TypeScript 模板
+> Vue3 + Vite + ElementPlus + TypeScript 模板
+
+## 文档
+
+[Vue3文档](https://staging-cn.vuejs.org/)
+[Vite文档](https://cn.vitejs.dev/)
+[ElementPlus文档](https://element-plus.org/zh-CN/)
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,9 +6,27 @@ const router = createRouter({
     {
       path: "/",
       name: "Index",
+      meta: {
+        title: "首页",
+      },
       component: () => import("../views/IndexView.vue"),
+    },
+    {
+      path: "/404",
+      name: "404",
+      meta: {
+        title: "404",
+      },
+      component: () => import("../views/NotFound.vue"),
+    },
+    {
+      path: "/:pathMatch(.*)",
+      redirect: "/404",
     },
   ],
 });
-
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | 主标题`;
+  next();
+});
 export default router;
